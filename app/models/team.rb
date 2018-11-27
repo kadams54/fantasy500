81dc32e415f7957e38c5c1603d3d04bbe73e0dad
Kyle Adams <kadams54@gmail.com>
2018-11-27 12:13:53 -0500

    Adds scoreboard to welcome page.

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,4 +2,8 @@ class Team < ApplicationRecord
   has_and_belongs_to_many :drivers
   validates :name, presence: true, length: {minimum: 2}
   validates :driver_ids, length: {maximum: 5}
+
+  def score
+    drivers.collect { |driver| driver.current_position }.reduce(:+)
+  end
 end

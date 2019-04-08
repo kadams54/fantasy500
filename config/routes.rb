6d8ec2b00742685156ce8e10987be258dc6965cb
Kyle Adams <kadams54@gmail.com>
2019-04-08 11:01:00 -0400

    Implements basic user support.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "welcome/index"
+  get "signup", to: 'users#new'
 
   resources :drivers
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_22_024204) do
+ActiveRecord::Schema.define(version: 2019_04_08_144614) do
 
   create_table "drivers", force: :cascade do |t|
     t.string "name"
@@ -51,6 +51,15 @@ ActiveRecord::Schema.define(version: 2018_11_22_024204) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end

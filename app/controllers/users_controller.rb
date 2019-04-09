82ac9a2062e35e81a8a40fe7c0e05bba80e84a38
Kyle Adams <kadams54@gmail.com>
2019-04-09 09:43:52 -0400

    Standards up!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,13 +13,13 @@ class UsersController < ApplicationController
       flash[:success] = "Welcome to the Fantasy 500!"
       redirect_to @user
     else
-      render 'new'      
+      render "new"
     end
   end
 
   private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,11 +1,11 @@
-require 'test_helper'
+require "test_helper"
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
     assert_select 'form[action="/signup"]'
     assert_no_difference "User.count" do
-      post signup_path, params: { user: { name: "", email: "user@invalid", password: "foo", password_confirmation: "bar"}}
+      post signup_path, params: {user: {name: "", email: "user@invalid", password: "foo", password_confirmation: "bar"}}
     end
     assert_template "users/new"
     assert_select "div.toast-error"
@@ -16,7 +16,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_select 'form[action="/signup"]'
     assert_difference "User.count", 1 do
-      post signup_path, params: { user: { name: "Ned Stark", email: "ned@example.com", password: "LannistersAreTheWorst", password_confirmation: "LannistersAreTheWorst"}}
+      post signup_path, params: {user: {name: "Ned Stark", email: "ned@example.com", password: "LannistersAreTheWorst", password_confirmation: "LannistersAreTheWorst"}}
     end
     follow_redirect!
     assert_template "users/show"

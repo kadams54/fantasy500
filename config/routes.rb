a916039774d2b5c18ce1e33606ae14debd64ad33
Kyle Adams <kadams54@gmail.com>
2019-04-11 23:56:34 -0400

    Implements basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
-  get "welcome/index"
-  get "signup", to: "users#new"
-  post "signup", to: "users#create"
+  root   "welcome#index"
+  get    "welcome/index"
+  get    "signup", to: "users#new"
+  post   "signup", to: "users#create"
+  get    "login" , to: "sessions#new"
+  post   "login" , to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
 
   resources :drivers
-
   resources :grids do
     resources :positions
   end
-
   resources :teams
-
   resources :users
-
-  root "welcome#index"
 end

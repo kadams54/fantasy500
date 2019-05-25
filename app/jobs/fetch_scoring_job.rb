cd578b962963021348b974a78edc3fcf108a59e4
Kyle Adams <kadams54@gmail.com>
2019-05-25 15:32:52 -0400

    Adds data safety to scoring checks.

--- app/jobs/fetch_scoring_job.rb
+++ app/jobs/fetch_scoring_job.rb
@@ -5,11 +5,15 @@ class FetchScoringJob < ApplicationJob
     response = Faraday.get "https://racecontrol.indycar.com/xml/timingscoring.json"
     scoring = JSON.parse(response.body)
     lap = scoring.dig("timing_results", "heartbeat", "lapNumber").to_i
-    Rails.logger.info("Adding scoring info for lap #{lap}")
-    grid = Grid.find_or_create_by(lap: lap)
-    scoring["timing_results"]["Item"].each do |result|
-      driver = Driver.find_by(number: result["no"])
-      grid.positions.build(place: result["overallRank"], driver: driver)
+    if lap && lap > 0
+      Rails.logger.info("Adding scoring info for lap #{lap}")
+      grid = Grid.find_or_create_by(lap: lap)
+      scoring["timing_results"]["Item"].each do |result|
+        driver = Driver.find_by(number: result["no"])
+        grid.positions.build(place: result["overallRank"], driver: driver)
+      end
+    else
+      Rails.logger.info("Skipping scoring; no lap info found")
     end
   end
 end

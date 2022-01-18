d295a2e0eb4b9e607a1518bc5af69a46febc6315
Kyle Adams <kadams54@users.noreply.github.com>
2022-01-18 17:34:22 -0500

    Assert team validity

--- test/models/team_test.rb
+++ test/models/team_test.rb
@@ -6,6 +6,15 @@ class TeamTest < ActiveSupport::TestCase
     @team = create(:team, user: user)
   end
 
+  test "should be valid" do
+    assert @team.valid?
+  end
+
+  test "name should be present" do
+    @team.name = "      "
+    assert_not @team.valid?
+  end
+
   test "#score" do
     grid = create(:grid, :start)
 

e49bed0fdcb3c3d956600061992355ca3f29cfcc
Kyle Adams <kadams54@users.noreply.github.com>
2022-01-18 17:27:46 -0500

    Test coverage on team model

--- test/models/team_test.rb
+++ test/models/team_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class TeamTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    user = create(:user, :active)
+    @team = create(:team, user: user)
+  end
+
+  test "#score" do
+    grid = create(:grid, :start)
+
+    driver_one = create(:driver, teams: [@team]) 
+    create(:position, place: 3, driver: driver_one, grid: grid)
+
+    driver_two = create(:driver, teams: [@team]) 
+    create(:position, place: 8, driver: driver_two, grid: grid)
+
+    assert_equal(11, @team.score)
+  end
 end

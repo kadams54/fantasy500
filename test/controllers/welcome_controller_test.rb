cc23d1598dd18ca2133e835f6e662de7d8deaa82
Kyle Adams <kadams54@users.noreply.github.com>
2022-02-17 22:07:31 -0500

    Complete coverage on the welcome controller

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,8 +1,18 @@
 require "test_helper"
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = create(:user, :active, password_digest: User.digest('password'))
+  end
+
   test "should get index" do
     get welcome_index_url
     assert_response :success
+  end
+
+  test "should redirect to dashboard if logged in" do
+    log_in_as @user
+    get welcome_index_url
+    assert_response :redirect
   end
 end

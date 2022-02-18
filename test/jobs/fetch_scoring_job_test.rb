ee6f5415c91d34f2175af98bc6bb749420ba6657
Kyle Adams <kadams54@users.noreply.github.com>
2022-02-17 22:06:33 -0500

    Complete coverage of the fetch scoring job

--- test/jobs/fetch_scoring_job_test.rb
+++ test/jobs/fetch_scoring_job_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class FetchScoringJobTest < ActiveJob::TestCase
+  def setup
+    ENV["SCORING_UPDATES"] = "enabled"
+    ENV["SCORING_EVENT_NAME"] = "INDYCAR Indianapolis 500"
+  end
+
   test "that scores have been fetched" do
     json = File.read("./test/fixtures/timingscoring.json")
     stub_request(:get, "https://racecontrol.indycar.com/xml/timingscoring.json").to_return(body: json)
@@ -13,6 +18,30 @@ class FetchScoringJobTest < ActiveJob::TestCase
     json = File.read("./test/fixtures/timingscoring-withcallback.json")
     stub_request(:get, "https://racecontrol.indycar.com/xml/timingscoring.json").to_return(body: json)
     assert_difference "Grid.current.count", 1 do
+      FetchScoringJob.perform_now()
+    end
+  end
+
+  test "that scores are skipped when updates disabled" do
+    ENV["SCORING_UPDATES"] = "disabled"
+    assert_no_difference "Grid.current.count" do
+      FetchScoringJob.perform_now()
+    end
+  end
+
+  test "that scores are skipped when wrong event" do
+    ENV["SCORING_EVENT_NAME"] = "wrong event"
+    json = File.read("./test/fixtures/timingscoring.json")
+    stub_request(:get, "https://racecontrol.indycar.com/xml/timingscoring.json").to_return(body: json)
+    assert_no_difference "Grid.current.count" do
+      FetchScoringJob.perform_now()
+    end
+  end
+
+  test "that scores are skipped when no laps" do
+    json = File.read("./test/fixtures/timingscoring-nolaps.json")
+    stub_request(:get, "https://racecontrol.indycar.com/xml/timingscoring.json").to_return(body: json)
+    assert_no_difference "Grid.current.count" do
       FetchScoringJob.perform_now()
     end
   end

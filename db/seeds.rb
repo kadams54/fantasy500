1ceadd7bb5349c91578030118f8169ea9bf00b62
Kyle Adams <kadams54@users.noreply.github.com>
2022-04-15 23:13:19 -0400

    Only seed needed users to improve performance

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(name: "Gandalf",
-             email: "gandalf@example.com",
-             password: "foobar",
-             password_confirmation: "foobar",
+User.create!(name: "Administrator",
+             email: "admin@example.com",
+             password: "1password",
+             password_confirmation: "1password",
              admin: true,
              activated: true,
              activated_at: Time.zone.now)
-
-99.times do |n|
-  name = Faker::Name.name
-  email = "example-#{n + 1}@railstutorial.org"
-  password = "password"
-  User.create!(name: name,
-               email: email,
-               password: password,
-               password_confirmation: password,
-               activated: true,
-               activated_at: Time.zone.now)
-end

5c16127661db801f3d064043d7328f83c56ae8ca
Kyle Adams <kadams54@users.noreply.github.com>
2022-04-22 23:22:14 -0400

    Test dashboard: create a team

--- playwright/integration/dashboard.spec.ts
+++ playwright/integration/dashboard.spec.ts
@@ -7,8 +7,20 @@ test.beforeEach(async ({ page }) => {
 
 test.describe("Dashboard", () => {
   test("should allow me to create a new team", async ({ page }) => {
-    await page.locator('.btn:has-text("Create a new team")').click();
+    await page.locator('.btn:has-text("Create a new team")').first().click();
     await expect(page).toHaveURL(/teams\/new/);
-    await page.locator("#team_name").fill(faker.vehicle.vehicle());
+    const name = faker.vehicle.vehicle();
+    await page.locator("#team_name").fill(name);
+    const checkbox =
+      "input[type='checkbox'][name='team[driver_ids][]'] + .form-icon";
+    await page.locator(`:nth-match(${checkbox}, 1)`).click();
+    await page.locator(`:nth-match(${checkbox}, 2)`).click();
+    await page.locator(`:nth-match(${checkbox}, 3)`).click();
+    await page.locator(`:nth-match(${checkbox}, 4)`).click();
+    await page.locator(`:nth-match(${checkbox}, 5)`).click();
+    await page.locator("input[type='submit']").click();
+    await expect(page).toHaveURL(/teams\/.*/);
+    await expect(page.locator(".title")).toHaveText(name);
+    await expect(page.locator(".driver")).toHaveCount(5);
   });
 });

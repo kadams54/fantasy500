87627233947e0962c2f1d6b59563017c59f5a2fc
Kyle Adams <kadams54@users.noreply.github.com>
2022-05-23 00:04:15 -0400

    Update to pull starting grid from data feeds

--- app/jobs/fetch_indy500_drivers_job.rb
+++ app/jobs/fetch_indy500_drivers_job.rb
@@ -1,46 +1,63 @@
+def fetch(url)
+  Faraday.default_adapter = :net_http
+  response = Faraday.get url
+  body = response.body.strip().sub(/\w+Callback\(/, "").chomp(");")
+  JSON.parse(body)
+end
+
+MAKE_MODEL = {
+  'H' => 'Honda',
+  'C' => 'Chevrolet',
+}
+
 class FetchIndy500DriversJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    doc = Nokogiri::HTML(Net::HTTP.get(URI.parse(ENV["GRID_URL"])))
-    if !doc
-      raise "Unable to fetch HTML"
-      return
+    grid = fetch ENV["GRID_API"]
+    drivers = fetch ENV["DRIVERS_API"]
+
+    event_name = grid.dig("timing_results", "heartbeat", "eventName")
+    if event_name != ENV["SCORING_EVENT_NAME"]
+      raise "Aborting driver fetch; wrong event: '#{event_name}', should be: '#{ENV["SCORING_EVENT_NAME"]}'"
     end
 
-    positions = doc.css("div[style*='liveGridPositions']")
-    if positions.length != 33
-      raise "HTML contained an unexpected number of positions"
-      return
+    session_type = grid.dig("timing_results", "heartbeat", "SessionType")
+    if session_type != "Q"
+      raise "Aborting driver fetch; scoring data is not for qualifying sesssion: #{session_type}"
     end
 
-    grid = Grid.current.find_or_create_by(lap: 0) 
-    positions.each_with_index do |position, idx|
-      # These selectors are gnarly because the IMS starting grid page currently
-      # has very little semantic markup. So I'm quarantining them all here.
-      # BEGIN: gnarly selectors
-      car_image = position.css("img[src*='Liveries']").attribute('src').value
-      driver_image = position.css("img[src*='Headshot']").attribute('src').value
-      qualifying_speed = position.xpath(".//div[@style='text-align: right;']/div[@class='row']/div[last()]/text()").text.to_f 
-      name = position.xpath(".//div[@style='text-align: right;']/p").text
-      endplate = position.xpath(".//img[contains(@src, 'Endplates')]/@src").text
-      logo = position.xpath(".//img[contains(@src, '_logo')]/@src").text
-      # END: gnarly selectors
-      Rails.logger.info("#{(idx + 1).to_s.rjust(2, ' ')}: #{name}")
-      begin
-        driver = Driver.create!(
-          name: name,
-          number: endplate.match(/Endplates\/(\d+)-/)[1],  # could fail if match is nil
-          make_model: logo.match(/img_(\w+)_logo/)[1].capitalize,  # could fail if match is nil
-          driver_image: driver_image,
-          car_image: car_image,
-          qualifying_speed: qualifying_speed,
-        )
-        grid.positions.create!(place: idx + 1, driver: driver)
-      rescue ActiveRecord::RecordNotUnique
-        Rails.logger.error("Driver #{name} already exists in the database.")
-        next
-      end
+    items = grid.dig("timing_results", "Item") || []
+    if items.length != 33
+      raise "Scoring contained an unexpected number of positions: #{items.length}"
+    end
+
+    driver_images = {}
+    car_images = {}
+    (drivers.dig("drivers", "driver") || []).each do |driver|
+      driver_images[driver["name"]] = driver["headshot"]
+      car_images[driver["number"]] = driver["carillustration"]
+    end
+
+    grid = Grid.current.find_or_create_by!(lap: 0)
+    items.each do |item|
+      name = "#{item["firstName"]} #{item["lastName"]}"
+      place = item["overallRank"]
+      Rails.logger.info("#{(place).to_s.rjust(2, ' ')}: #{name}")
+      driver = Driver.current.create_with(
+        number: item["no"],
+        make_model: MAKE_MODEL[item["equipment"].split('/').second],
+        driver_image: driver_images[name],
+        car_image: car_images[item["no"]],
+        qualifying_speed: item.dig("AverageSpeed"),
+        team: item["team"],
+      ).find_or_create_by!(
+        name: name,
+      )
+      Rails.logger.info("🤖 Driver: [#{driver.id}] #{driver.name}")
+      grid.positions
+        .create_with(driver: driver)
+        .find_or_create_by!(place: place)
     end
   end
 end

e7f528b0ca78e4daf4ee5eba7ebade5d5a34efa0
Kyle Adams <kadams54@users.noreply.github.com>
2022-05-24 02:02:19 -0400

    Rollup race data from years past
    Removing all of the intermediary grids for past races frees up quite a
    few records to stay under Heroku's free limit.

--- app/models/grid.rb
+++ app/models/grid.rb
@@ -3,5 +3,6 @@ class Grid < ApplicationRecord
   has_many :drivers, through: :positions
   validates :lap, presence: true
   scope :current, ->() {where(year: Time.current.year)}
+  scope :past, ->() {where.not(year: Time.current.year)}
   attribute :year, :integer, default: -> {Time.current.year.to_i}
 end
